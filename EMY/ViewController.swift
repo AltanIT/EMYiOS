b02074c8789d9786b8f56afb0c64a847b8c087d2
Carlos Pimentel <carlosarturopimentel@macbook-pro-de-carlos.local>
2019-01-21 16:25:35 -0600

    Initial Commit

---
+++ EMY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMY
+//
+//  Created by Carlos Arturo Pimentel on 21/01/19.
+//  Copyright © 2019 EMY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
