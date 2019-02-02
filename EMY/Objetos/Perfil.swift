d2683237722a26728af27337f57445181a0093ba
Carlos Pimentel <carlosarturopimentel@macbook-pro-de-carlos.local>
2019-02-02 12:25:58 -0600

    pods

---
+++ EMY/Objetos/Perfil.swift
@@ -0,0 +1,14 @@
+//
+//  Perfil.swift
+//  EMY
+//
+//  Created by Carlos Arturo Pimentel on 1/31/19.
+//  Copyright © 2019 EMY. All rights reserved.
+//
+
+import UIKit
+
+class Perfil{
+
+  
+}
---
+++ Pods/Target Support Files/Pods-EMYUITests/Pods-EMYUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EMYUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EMYUITests
+@end
